471473f7e7e04b55acc1564dfd34747ecf4e0d47
eduarbo <eduarbo@gmail.com>
2011-08-04 19:53:35 -0500

    added images dir

---
+++ sproutcore/sample/tmp/debug/build/static/sample/en/current/source/models/task_model.js
@@ -0,0 +1,20 @@
+// ==========================================================================
+// Project:   Sample.Task
+// Copyright: @2011 My Company, Inc.
+// ==========================================================================
+/*globals Sample */
+
+/** @class
+
+  (Document your Model here)
+
+  @extends SC.Record
+  @version 0.1
+*/
+Sample.Task = SC.Record.extend(
+/** @scope Sample.Task.prototype */ {
+
+  title      : SC.Record.attr(String),
+
+}) ;
+; if ((typeof SC !== 'undefined') && SC && SC.Module && SC.Module.scriptDidLoad) SC.Module.scriptDidLoad('sample');
